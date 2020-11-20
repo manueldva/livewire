1e155a057391a8a348f1b2af71d72425e83b325b
Manuel David Avila <mavila@mdh.gob.ar>
2020-11-20 19:24:14 -0300

    first commit

---
+++ app/Http/Livewire/PostComponet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PostComponet extends Component
+{
+    public function render()
+    {
+        return view('livewire.post-componet');
+    }
+}
---
+++ resources/views/livewire/post-componet.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+
+    <h1>Titulo de prueba</h1>
+</div>
